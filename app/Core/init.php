28d9d041b2eedcf47c7b06b3b27125aedfb62bed
nickpsal <nickpsal@gmail.com>
2023-03-09 14:59:42 +0200

    commit

--- app/Core/init.php
+++ app/Core/init.php
@@ -9,7 +9,5 @@
     require "App.php";
     // auyoload model files
     spl_autoload_register(function($classname) {
-        //$classname = explode("\\", $classname);
-        //$classname = end($classname);
         require "../app/Models/" . ucfirst($classname) . ".php";
     });
